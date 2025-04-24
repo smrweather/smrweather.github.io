43b6039c47da622e3605f0e9edae30cd339f767c
smrweather <meosanmateo20@gmail.com>
2025-04-24 22:35:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -275,7 +275,7 @@ fetch(apiUrl)
 // }
 
 
-Main weather updater
+//Main weather updater
 function refreshSanMateoWeather() {
 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
   .then(res => res.json())
